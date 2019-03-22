c4b9fcde5eed3533332b8bb384159ef1e63324b4
xuxuefeng <jxxuxuefeng@qq.com>
2019-03-22 09:15:15 +0800

    增加antd和例子文件

--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -7,7 +7,7 @@ const common = require("./webpack.common.js");
 // 清除dist文件夹
 const CleanWebpackPlugin = require("clean-webpack-plugin");
 // 打包分析
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = merge(common, {
   // 模式 开发环境 development  正式环境 production
@@ -17,7 +17,7 @@ module.exports = merge(common, {
     // 清除dist文件夹
     new CleanWebpackPlugin(),
     // 打包分析插件
-    new BundleAnalyzerPlugin()
+    // new BundleAnalyzerPlugin()
   ],
   // 代码分割
   optimization: {
--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 import React, { Component } from "react";
 import { hot } from "react-hot-loader/root";
 import _ from "lodash";
-
+import { DatePicker, Input, Select } from "antd";
+const Option = Select.Option;
 class App extends Component {
   constructor(props) {
     super(props);
   }
-  
+
   render() {
-    console.log(_.join([1,2,3444,4],'***'))
+    console.log(_.join([1, 2, 3444, 4], "***"));
     return (
       <div>
-        <input />
-        <input value="test HMR33" />
-        <button>按钮</button>
+        <Select defaultValue="lucy" style={{ width: 120 }}>
+          <Option value="jack">Jack</Option>
+          <Option value="lucy">Lucy</Option>
+          <Option value="disabled" disabled>
+            Disabled
+          </Option>
+          <Option value="Yiminghe">yiminghe</Option>
+        </Select>
+        <DatePicker />
+        <Input />
+        <input value="test HMR" />
+        <button>按钮33</button>
       </div>
     );
   }
